42744251ce8a4593148ffe0800c14fa4de7f51ba
X07U <36509249+X07U@users.noreply.github.com>
2024-12-26 21:32:39 +0100

    Update configuration.nix

--- nixconfig/configuration.nix
+++ nixconfig/configuration.nix
@@ -280,11 +280,27 @@ services.mpd = {
 	};
 
   programs.virt-manager.enable = true;
-
   users.groups.libvirtd.members = ["xozu"];
-
   virtualisation.libvirtd.enable = true;
-
   virtualisation.spiceUSBRedirection.enable = true;
+
+zramSwap.enable = true;
+fileSystems."/persist".neededForBoot = true;
+  environment.persistence."/persist" = {
+    hideMounts = true;
+    directories = [
+      "/etc/nixos"
+      "/var/log"
+      "/var/lib/bluetooth"
+      "/var/lib/nixos"
+      "/var/lib/systemd/coredump"
+      "/etc/NetworkManager/system-connections"
+      { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
+    ];
+    files = [
+      "/etc/machine-id"
+      { file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
+    ];
+  };
 
 }

b7ea540b42f6d7b6785cb74abade0758f239d32c
X07U <36509249+X07U@users.noreply.github.com>
2024-12-29 22:08:51 +0000

    Update configuration.nix

--- nixconfig/configuration.nix
+++ nixconfig/configuration.nix
@@ -194,9 +194,10 @@ fd
 # ✨			✨ USER ✨			✨
 # ┗━━━━━━━━━━━━━━━━━━━━━━━━┛ 
 
+users.mutableUsers = false;
   users.users.xozu = {
     isNormalUser = true;
-    initialPassword = "123";
+    hashedPasswordFile = "/persist/xozupass";
     extraGroups = [ "networkmanager" "wheel" ];
     shell = pkgs.zsh;
   };
@@ -285,37 +286,32 @@ services.mpd = {
   virtualisation.libvirtd.enable = true;
   virtualisation.spiceUSBRedirection.enable = true;
 
-fileSystems."/" = {
-    device = "none";
-    fsType = "tmpfs";
-    options = [ "defaults" "size=25%" "mode=755" ];
-  };
+boot.initrd.postDeviceCommands = lib.mkAfter ''
+    mkdir /btrfs_tmp
+    mount /dev/root_vg/root /btrfs_tmp
+    if [[ -e /btrfs_tmp/root ]]; then
+        mkdir -p /btrfs_tmp/old_roots
+        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+        mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+    fi
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-partlabel/disk-main-boot";
-    fsType = "vfat";
-    options = [ "fmask=0077" "dmask=0077" ];
-  };
+    delete_subvolume_recursively() {
+        IFS=$'\n'
+        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+            delete_subvolume_recursively "/btrfs_tmp/$i"
+        done
+        btrfs subvolume delete "$1"
+    }
 
-  fileSystems."/home" = {
-    device = "/dev/disk/by-partlabel/disk-main-btrfs";
-    fsType = "btrfs";
-    options = [ "subvol=home" "noatime" ];
-  };
+    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+        delete_subvolume_recursively "$i"
+    done
 
-  fileSystems."/nix" = {
-    device = "/dev/disk/by-partlabel/disk-main-btrfs";
-    fsType = "btrfs";
-    options = [ "subvol=nix" "noatime" ];
-  };
+    btrfs subvolume create /btrfs_tmp/root
+    umount /btrfs_tmp
+  '';
 
-  fileSystems."/persist" = {
-    neededForBoot = true;
-    device = "/dev/disk/by-partlabel/disk-main-btrfs";
-    fsType = "btrfs";
-    options = [ "subvol=persist" "noatime" ];
-  };
-
+fileSystems."/persist".neededForBoot = true;
 zramSwap.enable = true;
   environment.persistence."/persist" = {
     hideMounts = true;

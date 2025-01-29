d309ee34112e52d9d4102e5512a6824d49bc1f31
X07U <36509249+X07U@users.noreply.github.com>
2025-01-29 20:12:22 +0100

    final

--- nixconfig/hardware-configuration.nix
+++ nixconfig/hardware-configuration.nix
@@ -9,25 +9,9 @@
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
-
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/1cfdc747-83bc-485f-821c-40af82d70b8f";
-      fsType = "btrfs";
-      options = [ "subvol=@" ];
-    };
-
-  fileSystems."/boot/efi" =
-    { device = "/dev/disk/by-uuid/612E-BD3B";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/04f8ecc0-a8f1-4da6-a2df-79e33fe1b7b0"; }
-    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
